7e770bcc0e3e6ebae5fcb26810b2928983798e6b
unknown <jamesfr8764@hotmail.com>
2024-06-09 11:40:02 -0600

    agregando validaciones

--- router/Publicacion.py
+++ router/Publicacion.py
@@ -18,32 +18,32 @@ async def get_publicaciones():
     return publicaciones
 
 
-@publicacion.post("/creaPublicacion/", response_model=PublicacionSchema)
-async def crear_publicacion(publicacion: PublicacionBase):
+@publicacion.post(path="/registrarPublicacion/")
+async  def crear_publicacione(publicacion: PublicacionBase):
     db_publicacion = PublicacionModel(**publicacion.dict())
     medico = session.query(MedicoModel).filter(MedicoModel.id == db_publicacion.medico_id).first()
     usuario = session.query(UsuarioModel).filter(UsuarioModel.id == medico.usuario_id).first()
     fecha_actual = datetime.datetime.now()
     fecha_publicacion = db_publicacion.fecha_publicacion.date()
 
-    if medico and db_publicacion.imagen != "" and 0 < len(db_publicacion.contenido) < 100 and fecha_actual.date() == fecha_publicacion:
-        try:
-            resultado_json = []
-            consulta_dict = {
-                "contenido": db_publicacion.contenido,
-                "titulo": db_publicacion.titulo,
-                "imagen": db_publicacion.imagen,
-                "fecha_publicacion": db_publicacion.fecha_publicacion,
-                "medico": usuario.nombre
-            }
+    if usuario and fecha_actual.date() == fecha_publicacion:
+        if 0 < len(db_publicacion.imagen) <= 100:
+            try:
+                resultado_json = []
+                consulta_dict = {
+                    "titulo": db_publicacion.titulo,
+                    "contenido": db_publicacion.contenido,
+                    "imagen": db_publicacion.imagen,
+                    "fecha_publicacion": fecha_publicacion,
+                    "medico": usuario.nombre
+                }
+                resultado_json.append(consulta_dict)
+                session.add(db_publicacion)
+                session.commit()
+                session.refresh(db_publicacion)
 
-            resultado_json.append(consulta_dict)
+                return resultado_json
+            except SQLAlchemyError as e:
+                session.rollback()
+                print("Error al registrar la consulta:", e)
 
-            session.add(db_publicacion)
-            session.commit()
-            session.refresh(db_publicacion)
-
-            return resultado_json
-        except SQLAlchemyError as e:
-            session.rollback()
-            print("Error al registrar la consulta:", e)
