ae2e80a262fc42102dc28dd612ffe4ca9a115c90
Lucho <luchogo170@gmail.com>
2024-06-11 20:04:22 -0500

    Se ordena conexion db

--- utils/dbAlchemy.py
+++ utils/dbAlchemy.py
@@ -11,22 +11,9 @@ load_dotenv()
 username = os.getenv("DB_USERNAME_L")
 password = os.getenv("DB_PASSWORD")
 database = os.getenv("DB_NAME")
-print(username, password == "NH2YBNCg0VA1aY6I", database)
-# username = os.getenv("root")
-# password = os.getenv("1234")
-# database = os.getenv("hpv_safe")
-# DB_URL = "mysql://root:1234@127.0.0.1/hpv_safe"
-
-
-# DB_URL = f"mysql+pymysql://TestABC:TestABC@127.0.0.1/MySql80"
-
-# DB_URL = f"mysql+pymysql://{username}:{password}@localhost:3306/{database}"
-# Intentar conectar a la base de datos
 try:
     DB_URL = f"mysql+pymysql://soft_admin:{password}@127.0.0.1/hpv_safe"
     engine = create_engine(DB_URL)
-    # engine = create_engine("mysql://root:123456789@127.0.0.1/hpv_safe")
-    # engine = create_engine("mysql+pymysql://root:1234@127.0.0.1/hpv_safe")
     Session = sessionmaker(bind=engine)
     session = Session()
     Base = declarative_base()
